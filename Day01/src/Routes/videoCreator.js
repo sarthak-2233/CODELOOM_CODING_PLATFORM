2b20e3c0af2f618b2a02ee098ccc0d420277d81f
sarthak-2233 <sarthak.sharma33333@gmail.com>
2026-05-10 02:05:20 +0530

    Remove middleware for video

--- Day01/src/Routes/videoCreator.js
+++ Day01/src/Routes/videoCreator.js
@@ -7,7 +7,7 @@ videoRouter.get("/create/:problemId",adminMiddleware,generateUploadSignature);
 videoRouter.post("/save",adminMiddleware,saveVideoMetadata);
 videoRouter.delete("/delete/:problemId",adminMiddleware,deleteVideo);
 
-videoRouter.get("/problem/:problemId", adminMiddleware, getVideoByProblem);
-videoRouter.get("/my-videos", adminMiddleware, getUserVideos);
+videoRouter.get("/problem/:problemId", getVideoByProblem);
+videoRouter.get("/my-videos",  getUserVideos);
 
 module.exports = videoRouter;
